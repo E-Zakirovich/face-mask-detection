06cac57d31444cdde23cf980f2d224fd7a2dbaea
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-08-14 18:40:20 +0500

    bounding box settings stored, it was little bit hard but I managed to solve bugs

--- data/annotations.py
+++ data/annotations.py
@@ -17,7 +17,7 @@ class Annotations:
     def __init__(self, xml_path : str, labels : str, classes : list, class_to_idx : dict):
         self.xml_path = xml_path
         self.classes = classes
-        self.class_to_idx = class_to_idx
+        self.class_to_id = class_to_idx
         self.labels = labels
 
     def __convertor(self):
@@ -50,8 +50,40 @@ class Annotations:
 
         # I need a loop to look each elements inside .xml file
         for obj in root.findall("object"):
-            ...
-        
+            class_name = obj.find("name").text.strip()
+
+            # identification of class id
+            if class_name not in self.class_to_id:
+                print(f"  [skip] Missing class {class_name}")
+                continue
+            class_id = self.class_to_id[class_name]
+
+            # get bounding box part
+            bounding_box = obj.find("bndbox")
+            if bounding_box is None:
+                print(f"  [skip] Missing <{bounding_box}> element in {xml_path}")
+                return False
+
+            # bounding box settings
+            x_min = bounding_box.find("xmin")
+            y_min = bounding_box.find("ymin")
+            x_max = bounding_box.find("xmax")
+            y_max = bounding_box.find("ymax")
+
+            # checking bounding box elements the elements
+            x_min = x_min.text if x_min is not None else None
+            y_min = y_min.text if y_min is not None else None
+            x_max = x_max.text if x_max is not None else None
+            y_max = y_max.text if y_max is not None else None
+
+            # convert bounding box coordinates to float value
+            x_minimum = float(x_min) if x_min is not None else None
+            y_minimum = float(y_min) if y_min is not None else None
+            x_maximum = float(x_max) if x_max is not None else None
+            y_maximum = float(y_max) if y_max is not None else None
+
+            
+
         return True
 
 
