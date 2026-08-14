6d326184aeea41f6435051da027edbf668b75c4d
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-08-14 22:06:56 +0500

    convertor is finished, also new parameters added inside of init.

--- data/annotations.py
+++ data/annotations.py
@@ -14,11 +14,12 @@ import xml.etree.ElementTree as Et
 
 
 class Annotations:
-    def __init__(self, xml_path : str, labels : str, classes : list, class_to_idx : dict):
+    def __init__(self, xml_path : str, labels : str, classes : list, class_to_idx : dict, verbose : bool):
         self.xml_path = xml_path
         self.classes = classes
         self.class_to_id = class_to_idx
         self.labels = labels
+        self.verbose = verbose
 
     def __convertor(self):
         tree = Et.parse(self.xml_path)
@@ -121,6 +122,45 @@ class Annotations:
 
         return True
 
-
+    """
+    __convertor() method will help me to read only one file, i need a method
+    that can read multiple files using _convertor(). So I created new method 
+    called convert.
+    """
     def convert(self):
-        ...
+
+        self.verbose = True
+        annotations_path = self.xml_path # path of annotations
+        os.makedirs(self.xml_path, exist_ok=True) # making a path for annotations
+
+        # here I am storing all path of .xml file inside of annotations_path which is actually ends with .xml!
+        xml_files = [f for f in os.listdir(annotations_path) if f.endswith(".xml")]
+
+        # check the number of .xml files
+        if self.verbose:
+            print(f"Found {len(xml_files)} xml files in {annotations_path}")
+
+        converted = 0
+        skipped = 0
+
+        # read all data from annotations and store them with .txt file extension
+        for file in xml_files:
+            path = os.path.join(annotations_path, file)
+
+            # I used try catch to avoid stop the program when there is a static error
+            try:
+                convert = self.__convertor() #
+                if convert:
+                    converted += 1
+                else:
+                    skipped += 1
+
+            except Exception as e:
+                print(f"  [skip] {e}")
+                skipped += 1
+
+        # show final result
+        if self.verbose:
+            print(f"Converted {converted} xml files in {annotations_path}")
+
+        return converted, skipped
