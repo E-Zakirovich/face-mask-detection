317198ff2aa4930b0779fd9ee37231a3da149509
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-08-15 15:57:29 +0500

    architecture of train.py file is done, also initializator is finished.

--- train.py
+++ train.py
@@ -6,13 +6,20 @@ I need to teach YOLO(You Only Look Once) with sth, so I created
 train.py file. I will train my model with dataset in main.py.
 """
 
-import torchvision
 from ultralytics import YOLO
-import argparse
-import os
-import yaml
 
 
 class Train:
-    def __init__(self):
+    def __init__(self, data_yaml : str, model_path : str, epochs : int, image_size: int, verbose : bool = True):
+        self.data_yaml = data_yaml
+        self.model_path = model_path
+        self.epochs = epochs
+        self.image_size = image_size
+        self.verbose = verbose
+        self.model = None
+
+    def load_model(self):
+        ...
+
+    def train(self):
         ...
