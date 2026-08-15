4f208312bbca6fb0054cf504a14d5282cb4fc2c9
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-08-15 20:29:56 +0500

    i stored all static settings variables to configs.py file

--- configs/configs.py
+++ configs/configs.py
@@ -1,3 +1,16 @@
 # directories
 annotations_path = "data/annotations"
 labels_path = "data/labels"
+data_yaml_path = "data/dataset.yaml"
+
+# class order -> integer id (must match dataset.yaml)
+classes = ["with_mask", "without_mask", "mask_weared_incorrect"]
+class_to_id = {name: i for i, name in enumerate(classes)}
+
+# train settings
+model_path = "yolov8n.pt"
+epochs = 50
+image_size = 640
+
+# misc
+verbose = True
